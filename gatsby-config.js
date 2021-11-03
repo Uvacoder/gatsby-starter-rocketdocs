625e769418f055131a99d573ef4af3aa5d970b5c
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-11-02 21:20:31 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
-    siteTitle: `Rocket Docs`,
-    defaultTitle: `Rocket Docs`,
-    siteTitleShort: `Rocket Docs`,
-    siteDescription: `Out of the box Gatsby Theme for creating documentation websites easily and quickly`,
-    siteUrl: `https://rocketdocs.netlify.app`,
-    siteAuthor: `@jpedroschmitz`,
-    siteImage: `/banner.png`,
+    siteTitle: `React-Hooks Cheatsheet`,
+    defaultTitle: `React-Hooks Cheatsheet`,
+    siteTitleShort: `React-Hooks Cheatsheet`,
+    siteDescription: `A custom made cheatsheet with a handful of react hooks`,
+    siteUrl: `https://gatsby-starter-rocketdocs.vercel.app`,
+    siteAuthor: `@uvacoder`,
+    siteImage: `/logo.png`,
     siteLanguage: `en`,
     themeColor: `#8257E6`,
     basePath: `/`,
@@ -18,15 +18,15 @@ module.exports = {
       options: {
         configPath: `src/config`,
         docsPath: `src/docs`,
-        repositoryUrl: `https://github.com/jpedroschmitz/rocketdocs`,
-        baseDir: `examples/gatsby-theme-docs`,
+        repositoryUrl: `https://github.com/uvacoder/gatsby-starter-docs`,
+        baseDir: `/`,
       },
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Rocket Docs`,
-        short_name: `Rocket Docs`,
+        name: `React-Hooks Cheatsheet`,
+        short_name: `React-Hooks Cheatsheet`,
         start_url: `/`,
         background_color: `#ffffff`,
         display: `standalone`,
@@ -44,7 +44,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-canonical-urls`,
       options: {
-        siteUrl: `https://rocketdocs.netlify.app`,
+        siteUrl: `https://gatsby-starter-rocketdocs.vercel.app`,
       },
     },
     `gatsby-plugin-offline`,
